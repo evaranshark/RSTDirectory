850d31c85154e1d348dcbca0268d8091ab6753c3
evaranshark <evarancorp@gmail.com>
2017-10-12 00:48:20 +0300

    Собрана форма

--- RST_Directory/MyForm.h
+++ RST_Directory/MyForm.h
@@ -34,6 +34,22 @@ namespace RST_Directory {
 				delete components;
 			}
 		}
+	private: System::Windows::Forms::TableLayoutPanel^  tableLayoutPanel1;
+	protected:
+	private: System::Windows::Forms::Panel^  panel1;
+	private: System::Windows::Forms::TextBox^  textBox1;
+	private: System::Windows::Forms::Label^  label1;
+	private: System::Windows::Forms::Panel^  panel2;
+	private: System::Windows::Forms::TextBox^  textBox2;
+	private: System::Windows::Forms::Label^  label2;
+	private: System::Windows::Forms::Panel^  panel3;
+	private: System::Windows::Forms::Panel^  panel4;
+	private: System::Windows::Forms::Panel^  panel5;
+	private: System::Windows::Forms::Button^  button1;
+	private: System::Windows::Forms::GroupBox^  groupBox1;
+	private: System::Windows::Forms::GroupBox^  groupBox2;
+
+
 
 	private:
 		/// <summary>
@@ -48,11 +64,193 @@ namespace RST_Directory {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			this->components = gcnew System::ComponentModel::Container();
-			this->Size = System::Drawing::Size(300,300);
-			this->Text = L"MyForm";
-			this->Padding = System::Windows::Forms::Padding(0);
+			this->tableLayoutPanel1 = (gcnew System::Windows::Forms::TableLayoutPanel());
+			this->panel1 = (gcnew System::Windows::Forms::Panel());
+			this->panel2 = (gcnew System::Windows::Forms::Panel());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
+			this->panel3 = (gcnew System::Windows::Forms::Panel());
+			this->panel4 = (gcnew System::Windows::Forms::Panel());
+			this->panel5 = (gcnew System::Windows::Forms::Panel());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->groupBox1 = (gcnew System::Windows::Forms::GroupBox());
+			this->groupBox2 = (gcnew System::Windows::Forms::GroupBox());
+			this->tableLayoutPanel1->SuspendLayout();
+			this->panel1->SuspendLayout();
+			this->panel2->SuspendLayout();
+			this->panel3->SuspendLayout();
+			this->panel4->SuspendLayout();
+			this->panel5->SuspendLayout();
+			this->SuspendLayout();
+			// 
+			// tableLayoutPanel1
+			// 
+			this->tableLayoutPanel1->Anchor = static_cast<System::Windows::Forms::AnchorStyles>((((System::Windows::Forms::AnchorStyles::Top | System::Windows::Forms::AnchorStyles::Bottom)
+				| System::Windows::Forms::AnchorStyles::Left)
+				| System::Windows::Forms::AnchorStyles::Right));
+			this->tableLayoutPanel1->AutoSize = true;
+			this->tableLayoutPanel1->ColumnCount = 2;
+			this->tableLayoutPanel1->ColumnStyles->Add((gcnew System::Windows::Forms::ColumnStyle(System::Windows::Forms::SizeType::Percent,
+				50)));
+			this->tableLayoutPanel1->ColumnStyles->Add((gcnew System::Windows::Forms::ColumnStyle(System::Windows::Forms::SizeType::Percent,
+				50)));
+			this->tableLayoutPanel1->Controls->Add(this->panel1, 0, 0);
+			this->tableLayoutPanel1->Controls->Add(this->panel2, 1, 0);
+			this->tableLayoutPanel1->Controls->Add(this->panel3, 0, 1);
+			this->tableLayoutPanel1->Controls->Add(this->panel4, 1, 1);
+			this->tableLayoutPanel1->Controls->Add(this->panel5, 0, 2);
+			this->tableLayoutPanel1->Location = System::Drawing::Point(0, 0);
+			this->tableLayoutPanel1->Name = L"tableLayoutPanel1";
+			this->tableLayoutPanel1->RowCount = 3;
+			this->tableLayoutPanel1->RowStyles->Add((gcnew System::Windows::Forms::RowStyle(System::Windows::Forms::SizeType::Percent, 25.7732F)));
+			this->tableLayoutPanel1->RowStyles->Add((gcnew System::Windows::Forms::RowStyle(System::Windows::Forms::SizeType::Percent, 74.22681F)));
+			this->tableLayoutPanel1->RowStyles->Add((gcnew System::Windows::Forms::RowStyle(System::Windows::Forms::SizeType::Absolute, 88)));
+			this->tableLayoutPanel1->Size = System::Drawing::Size(756, 485);
+			this->tableLayoutPanel1->TabIndex = 0;
+			// 
+			// panel1
+			// 
+			this->panel1->Controls->Add(this->textBox1);
+			this->panel1->Controls->Add(this->label1);
+			this->panel1->Dock = System::Windows::Forms::DockStyle::Fill;
+			this->panel1->Location = System::Drawing::Point(3, 3);
+			this->panel1->Name = L"panel1";
+			this->panel1->Size = System::Drawing::Size(372, 96);
+			this->panel1->TabIndex = 0;
+			// 
+			// panel2
+			// 
+			this->panel2->Anchor = static_cast<System::Windows::Forms::AnchorStyles>((((System::Windows::Forms::AnchorStyles::Top | System::Windows::Forms::AnchorStyles::Bottom)
+				| System::Windows::Forms::AnchorStyles::Left)
+				| System::Windows::Forms::AnchorStyles::Right));
+			this->panel2->Controls->Add(this->textBox2);
+			this->panel2->Controls->Add(this->label2);
+			this->panel2->Location = System::Drawing::Point(381, 3);
+			this->panel2->Name = L"panel2";
+			this->panel2->Size = System::Drawing::Size(372, 96);
+			this->panel2->TabIndex = 1;
+			// 
+			// label1
+			// 
+			this->label1->Anchor = static_cast<System::Windows::Forms::AnchorStyles>(((System::Windows::Forms::AnchorStyles::Bottom | System::Windows::Forms::AnchorStyles::Left)
+				| System::Windows::Forms::AnchorStyles::Right));
+			this->label1->AutoSize = true;
+			this->label1->Location = System::Drawing::Point(93, 44);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(89, 13);
+			this->label1->TabIndex = 0;
+			this->label1->Text = L"Данные приёма";
+			this->label1->TextAlign = System::Drawing::ContentAlignment::MiddleCenter;
+			// 
+			// label2
+			// 
+			this->label2->Anchor = static_cast<System::Windows::Forms::AnchorStyles>(((System::Windows::Forms::AnchorStyles::Bottom | System::Windows::Forms::AnchorStyles::Left)
+				| System::Windows::Forms::AnchorStyles::Right));
+			this->label2->AutoSize = true;
+			this->label2->Location = System::Drawing::Point(93, 44);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(98, 13);
+			this->label2->TabIndex = 0;
+			this->label2->Text = L"Данные отправки";
+			this->label2->TextAlign = System::Drawing::ContentAlignment::MiddleCenter;
+			// 
+			// textBox1
+			// 
+			this->textBox1->Anchor = static_cast<System::Windows::Forms::AnchorStyles>(((System::Windows::Forms::AnchorStyles::Bottom | System::Windows::Forms::AnchorStyles::Left)
+				| System::Windows::Forms::AnchorStyles::Right));
+			this->textBox1->Location = System::Drawing::Point(96, 62);
+			this->textBox1->Name = L"textBox1";
+			this->textBox1->Size = System::Drawing::Size(180, 20);
+			this->textBox1->TabIndex = 1;
+			// 
+			// textBox2
+			// 
+			this->textBox2->Anchor = static_cast<System::Windows::Forms::AnchorStyles>(((System::Windows::Forms::AnchorStyles::Bottom | System::Windows::Forms::AnchorStyles::Left)
+				| System::Windows::Forms::AnchorStyles::Right));
+			this->textBox2->Location = System::Drawing::Point(96, 62);
+			this->textBox2->Name = L"textBox2";
+			this->textBox2->Size = System::Drawing::Size(180, 20);
+			this->textBox2->TabIndex = 1;
+			// 
+			// panel3
+			// 
+			this->panel3->Controls->Add(this->groupBox1);
+			this->panel3->Dock = System::Windows::Forms::DockStyle::Fill;
+			this->panel3->Location = System::Drawing::Point(3, 105);
+			this->panel3->Name = L"panel3";
+			this->panel3->Size = System::Drawing::Size(372, 288);
+			this->panel3->TabIndex = 2;
+			// 
+			// panel4
+			// 
+			this->panel4->Controls->Add(this->groupBox2);
+			this->panel4->Dock = System::Windows::Forms::DockStyle::Fill;
+			this->panel4->Location = System::Drawing::Point(381, 105);
+			this->panel4->Name = L"panel4";
+			this->panel4->Size = System::Drawing::Size(372, 288);
+			this->panel4->TabIndex = 3;
+			// 
+			// panel5
+			// 
+			this->panel5->Controls->Add(this->button1);
+			this->panel5->Dock = System::Windows::Forms::DockStyle::Fill;
+			this->panel5->Location = System::Drawing::Point(3, 399);
+			this->panel5->Name = L"panel5";
+			this->panel5->Size = System::Drawing::Size(372, 83);
+			this->panel5->TabIndex = 4;
+			// 
+			// button1
+			// 
+			this->button1->Anchor = static_cast<System::Windows::Forms::AnchorStyles>(((System::Windows::Forms::AnchorStyles::Top | System::Windows::Forms::AnchorStyles::Bottom)
+				| System::Windows::Forms::AnchorStyles::Left));
+			this->button1->Location = System::Drawing::Point(9, 14);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(173, 58);
+			this->button1->TabIndex = 0;
+			this->button1->Text = L"Очистить";
+			this->button1->UseVisualStyleBackColor = true;
+			// 
+			// groupBox1
+			// 
+			this->groupBox1->Dock = System::Windows::Forms::DockStyle::Fill;
+			this->groupBox1->Location = System::Drawing::Point(0, 0);
+			this->groupBox1->Name = L"groupBox1";
+			this->groupBox1->Size = System::Drawing::Size(372, 288);
+			this->groupBox1->TabIndex = 0;
+			this->groupBox1->TabStop = false;
+			this->groupBox1->Text = L"Приём";
+			// 
+			// groupBox2
+			// 
+			this->groupBox2->Dock = System::Windows::Forms::DockStyle::Fill;
+			this->groupBox2->Location = System::Drawing::Point(0, 0);
+			this->groupBox2->Name = L"groupBox2";
+			this->groupBox2->Size = System::Drawing::Size(372, 288);
+			this->groupBox2->TabIndex = 0;
+			this->groupBox2->TabStop = false;
+			this->groupBox2->Text = L"Отправка";
+			// 
+			// MyForm
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(755, 483);
+			this->Controls->Add(this->tableLayoutPanel1);
+			this->Name = L"MyForm";
+			this->Text = L"MyForm";
+			this->tableLayoutPanel1->ResumeLayout(false);
+			this->panel1->ResumeLayout(false);
+			this->panel1->PerformLayout();
+			this->panel2->ResumeLayout(false);
+			this->panel2->PerformLayout();
+			this->panel3->ResumeLayout(false);
+			this->panel4->ResumeLayout(false);
+			this->panel5->ResumeLayout(false);
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
 		}
 #pragma endregion
 	};
