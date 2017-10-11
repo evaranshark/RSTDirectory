ead1cc0396bbd928f5c931ab13e4ee8d6c1c038f
evaranshark <evarancorp@gmail.com>
2017-10-11 22:55:55 +0300

    Добавьте файлы проекта.

---
+++ RST_Directory/MyForm.cpp
@@ -0,0 +1,15 @@
+#include "MyForm.h"
+
+using namespace System;
+using namespace System::Windows::Forms;
+
+
+[STAThread]
+void Main(array<String^>^ args)
+{
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+
+	RST_Directory::MyForm form;
+	Application::Run(%form);
+}
